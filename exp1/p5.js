e3c213c0f230b36cb2ae77f76b1a9d2f51bbc308
Sahil390 <snarang445566@gmail.com>
2025-08-20 09:52:21 +0000

    Fix file path and restore server port to 3000 in p5.js

--- exp1/p5.js
+++ exp1/p5.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer(function(req,res) {
-    fs.readFile('tfile.txt','utf8', function(err,data){
+    fs.readFile('./tfile.txt','utf8', function(err,data){
         if(err) {    
-            console.error("Error ",err.message);
+            // console.error("Error ",err.message);
             res.write("File not found or error reading file.");
             return;
         }
@@ -12,4 +12,4 @@ const server = http.createServer(function(req,res) {
         res.end();
     });
 });
-server.listen(6000)
+server.listen(3000);
